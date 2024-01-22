65b6bd6debd85c8dea7965c747583f71bdf30c73
duckfever <benjor@gmail.com>
2024-01-22 12:39:12 +0000

    updated to super bare-bones

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-const density = "XXIANxxxian{}();.,_   ".split("").reverse().join("");
-
+// const density = "xian".split("").reverse().join("");
+const density = " .:-=+*#%@";
 let video;
 let asciiDiv;
 
 function setup() {
 	asciiDiv = createDiv();
-	asciiDiv.parent('p5-container'); // replace 'your-div-id' with the id of your div
+	asciiDiv.parent("p5-container"); // replace 'your-div-id' with the id of your div
 	asciiDiv.id("moving-bg");
 	noCanvas();
-	video = createVideo("xian7_.mp4");
+	video = createVideo("tree-python.mp4");
+	// video = createVideo("sandworm.mp4");
 	let { height, width } = video;
-	const true_width = width / 3;
-	const true_height = height / 5;
+	const true_width = width / 4;
+	const true_height = height / 2;
 	console.log({ true_width, true_height });
 	video.size(true_width, true_height);
 	video.elt.muted = true;
@@ -41,13 +42,10 @@ function draw() {
 			const len = density.length;
 			const charIndex = floor(map(avg, 0, 255, 0, len));
 			let c;
+			// if (charIndex > density.length) console.log(charIndex)
 			c = density.charAt(charIndex); // Use a character based on pixel brightness
-			const color = `rgb(0,${g},${b})`;
-			if (c == " ") {
-				asciiImage += "&nbsp;";
-			} else {
+			const color = `rgb(${r},${g},${b})`;
 				asciiImage += '<span style="color:' + color + '">' + c + "</span>";
-			}
 		}
 		asciiImage += "<br/>";
 	}
